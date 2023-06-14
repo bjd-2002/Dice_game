0a21e1c0c6541d4e6d7160762d6355719e8a9cbd
bjd-2002 <111186404+bjd-2002@users.noreply.github.com>
2023-06-14 14:00:22 +0530

    added

---
+++ index.js
@@ -0,0 +1,37 @@
+window.onload = () => {
+  let reloading = sessionStorage.getItem("reloading");
+  if (reloading) {
+      sessionStorage.removeItem("reloading");
+      myFunction();
+  }
+  sessionStorage.setItem("reloading", "true");
+};
+let dice = document.querySelectorAll("img");
+let dicegame = () => {
+  dice.forEach(function(die){
+    die.classList.add("apply-shake");
+  });
+  setTimeout(function(){
+    dice.forEach(function(die){
+      die.classList.remove("apply-shake");
+    });
+    let randomNumber1 = 1 + Math.floor(6 * Math.random());
+    let dice1 = "images/dice" + randomNumber1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", dice1);
+
+    let randomNumber2 = 1 + Math.floor(6 * Math.random());
+    let dice2 = "images/dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", dice2);
+    setTimeout(function(){
+      if (randomNumber1 > randomNumber2) document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+      else if (randomNumber2 > randomNumber1) document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+      else document.querySelector("h1").innerHTML = "Draw!";
+    },
+    0
+    );
+    
+  },
+  250
+  );
+  
+};
